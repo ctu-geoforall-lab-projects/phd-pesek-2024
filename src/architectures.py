2adaf3457692c6d4caff507adaaa32c106e0b1fa
pesekon2 <pesej.ondrek@gmail.com>
2021-01-24 00:12:20 +0100

    refactor get_unet() - less letters, more love

--- src/architectures.py
+++ src/architectures.py
@@ -19,118 +19,54 @@ def get_unet(nr_classes, nr_bands=12, nr_filters=16, bn=True, dilation_rate=1,
     :param tensor_shape: shape of the first two dimensions of input tensors
     :return: U-Net model
     """
+    concat_layers = []
+
     inputs = Input((tensor_shape[0], tensor_shape[1], nr_bands))
+    x = inputs
 
-    # TODO: avoid duplicity - make more systematic
-    conv1 = Conv2D(nr_filters * 1, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(inputs)
-    if bn:
-        conv1 = BatchNormalization()(conv1)
+    for i in range(4):
+        for j in range(2):
+            x = Conv2D(nr_filters * (2 ** i),
+                       (3, 3),
+                       activation='relu',
+                       padding='same',
+                       dilation_rate=dilation_rate)(x)
+            if bn:
+                x = BatchNormalization()(x)
 
-    conv1 = Conv2D(nr_filters * 1, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(conv1)
-    if bn:
-        conv1 = BatchNormalization()(conv1)
+        concat_layers.append(x)
+        x = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(x)
 
-    pool1 = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(conv1)
+    for i in range(4, 0, -1):
+        for j in range(2):
+            x = Conv2D(nr_filters * (2 ** i),
+                       (3, 3),
+                       activation='relu',
+                       padding='same',
+                       dilation_rate=dilation_rate)(x)
+            if bn:
+                x = BatchNormalization()(x)
 
-    conv2 = Conv2D(nr_filters * 2, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(pool1)
-    if bn:
-        conv2 = BatchNormalization()(conv2)
+        x = Concatenate(axis=3)([UpSampling2D(size=(2, 2))(x),
+                                 concat_layers[i - 1]])
 
-    conv2 = Conv2D(nr_filters * 2, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(conv2)
-    if bn:
-        conv2 = BatchNormalization()(conv2)
+    conv9 = x
 
-    pool2 = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(conv2)
-
-    conv3 = Conv2D(nr_filters * 4, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(pool2)
-    if bn:
-        conv3 = BatchNormalization()(conv3)
-
-    conv3 = Conv2D(nr_filters * 4, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(conv3)
-    if bn:
-        conv3 = BatchNormalization()(conv3)
-
-    pool3 = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(conv3)
-
-    conv4 = Conv2D(nr_filters * 8, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(pool3)
-    if bn:
-        conv4 = BatchNormalization()(conv4)
-
-    conv4 = Conv2D(nr_filters * 8, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(conv4)
-    if bn:
-        conv4 = BatchNormalization()(conv4)
-
-    pool4 = MaxPooling2D(pool_size=(2, 2), data_format='channels_last')(conv4)
-
-    conv5 = Conv2D(nr_filters * 16, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(pool4)
-    if bn:
-        conv5 = BatchNormalization()(conv5)
-
-    conv5 = Conv2D(nr_filters * 16, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(conv5)
-    if bn:
-        conv5 = BatchNormalization()(conv5)
-
-    up6 = Concatenate(axis=3)([UpSampling2D(size=(2, 2))(conv5), conv4])
-
-    conv6 = Conv2D(nr_filters * 8, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(up6)  # (conv4)  # (up6)
-    if bn:
-        conv6 = BatchNormalization()(conv6)
-
-    conv6 = Conv2D(nr_filters * 8, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(conv6)
-    if bn:
-        conv6 = BatchNormalization()(conv6)
-
-    up7 = Concatenate(axis=3)([UpSampling2D(size=(2, 2))(conv6), conv3])
-
-    conv7 = Conv2D(nr_filters * 4, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(up7)  # (conv3)  # (up7)
-    if bn:
-        conv7 = BatchNormalization()(conv7)
-
-    conv7 = Conv2D(nr_filters * 4, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(conv7)
-    if bn:
-        conv7 = BatchNormalization()(conv7)
-
-    up8 = Concatenate(axis=3)([UpSampling2D(size=(2, 2))(conv7), conv2])
-
-    conv8 = Conv2D(nr_filters * 2, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(up8)
-    if bn:
-        conv8 = BatchNormalization()(conv8)
-
-    conv8 = Conv2D(nr_filters * 2, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(conv8)
-    if bn:
-        conv8 = BatchNormalization()(conv8)
-
-    up9 = Concatenate(axis=3)([UpSampling2D(size=(2, 2))(conv8), conv1])
-
-    conv9 = Conv2D(nr_filters * 1, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(up9)
-    if bn:
-        conv9 = BatchNormalization()(conv9)
-
-    conv9 = Conv2D(nr_filters * 1, (3, 3), activation='relu', padding='same',
-                   dilation_rate=dilation_rate)(conv9)
-    if bn:
-        conv9 = BatchNormalization()(conv9)
+    for i in range(2):
+        conv9 = Conv2D(nr_filters * 1,
+                       (3, 3),
+                       activation='relu',
+                       padding='same',
+                       dilation_rate=dilation_rate)(conv9)
+        if bn:
+            conv9 = BatchNormalization()(conv9)
 
     # number of classes
-    conv10 = Conv2D(nr_classes, (1, 1), activation='softmax',
-                    padding='same', dilation_rate=dilation_rate)(conv9)
+    conv10 = Conv2D(nr_classes,
+                    (1, 1),
+                    activation='softmax',
+                    padding='same',
+                    dilation_rate=dilation_rate)(conv9)
 
     model = Model(inputs=inputs, outputs=conv10)
 
