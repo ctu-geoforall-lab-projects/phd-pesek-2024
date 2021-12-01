5a83abb20c3dba3971cb642aee362e8391b6afdd
pesekon2 <pesej.ondrek@gmail.com>
2021-12-01 13:22:13 +0200

    update docstrings

--- src/cnn_exceptions.py
+++ src/cnn_exceptions.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 
 class ModelConfigError(Exception):
-    """Model to be raised in the case of wrong parameters to build up a model.
-    """
+    """Error to be raised in the case of wrong parameters of a model config."""
 
     pass
 
 
 class DatasetError(Exception):
-    """Model to be raised in the case of problems with the dataset.
-    """
+    """Error to be raised in the case of problems with the dataset."""
 
     pass
 
 
 class LayerDefinitionError(Exception):
-    """Model to be raised in the case of problems with the layer definition.
-    """
+    """Error to be raised in the case of problems with the layer definition."""
 
     pass
