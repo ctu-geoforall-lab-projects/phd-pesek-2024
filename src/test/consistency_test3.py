2575f3c19e9b4cd94aa0de09271736c82c3fc2fb
ondrej <pesej.ondrek@gmail.com>
2022-11-01 18:44:27 +0100

    GH action: TF 2.10.0, gdal343, cpu1, keras_seed, entropy

--- src/test/consistency_test3.py
+++ src/test/consistency_test3.py
@@ -40,7 +40,7 @@ def are_dir_trees_equal(dir1, dir2):
 
 
 class TestCmd:
-    def test_001_clouds(self):
+    def test_001_clouds(self, capsys):
         """Test the consistency of a small cloud classification sample."""
 
         # TODO: Add augment, continue, val_losses, architectures
